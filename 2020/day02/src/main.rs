24c08c4d566b624f0560630626880fab41e5fbe9
Dat Truong <mr.anhdat@gmail.com>
2020-12-02 11:21:02 +0100

    Use regex for parsing

--- 2020/day02/src/main.rs
+++ 2020/day02/src/main.rs
@@ -1,4 +1,10 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use std::fs;
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"(\d+)-(\d+) (\w): (\w*)").unwrap();
+}
 
 fn list_of_strings_from_file() -> Vec<String> {
     let filename = "./input";
@@ -30,22 +36,22 @@ impl Policy {
     }
 }
 
-fn is_valid(line: &str) -> bool {
-    let comps: Vec<&str> = line.split(" ").collect();
-    let comps_2: Vec<&str> = comps[0].split("-").collect();
-    let comps_3: Vec<&str> = comps[1].split(":").collect();
+fn is_valid(line: &str) -> Option<bool> {
+    // 2-4 p: vpkpp
+    let comps = RE.captures(line)?;
     let policy = Policy {
-        min: comps_2[0].parse().unwrap(),
-        max: comps_2[1].parse().unwrap(),
-        character: comps_3[0].chars().next().unwrap(),
+        min: comps.get(1)?.as_str().parse().ok()?,
+        max: comps.get(2)?.as_str().parse().ok()?,
+        character: comps.get(3)?.as_str().chars().next()?,
     };
-    let password = comps[2];
-    policy.is_valid(password)
+    let password = comps.get(4)?.as_str();
+
+    Some(policy.is_valid(password))
 }
 
 fn part_1() {
     let lines = list_of_strings_from_file();
-    let count = lines.iter().filter(|l| is_valid(l)).count();
+    let count = lines.iter().filter(|l| is_valid(l).unwrap()).count();
     println!("{}", count);
 }
 
@@ -63,23 +69,21 @@ impl Policy2 {
     }
 }
 
-fn is_valid2(line: &str) -> bool {
-    let comps: Vec<&str> = line.split(" ").collect();
-    let comps_2: Vec<&str> = comps[0].split("-").collect();
-    let comps_3: Vec<&str> = comps[1].split(":").collect();
+fn is_valid2(line: &str) -> Option<bool> {
+    let comps = RE.captures(line)?;
     let policy = Policy2 {
-        pos1: comps_2[0].parse().unwrap(),
-        pos2: comps_2[1].parse().unwrap(),
-        character: comps_3[0].chars().next().unwrap(),
+        pos1: comps.get(1)?.as_str().parse().ok()?,
+        pos2: comps.get(2)?.as_str().parse().ok()?,
+        character: comps.get(3)?.as_str().chars().next()?,
     };
-    let password = comps[2];
+    let password = comps.get(4)?.as_str();
 
-    policy.is_valid(password)
+    Some(policy.is_valid(password))
 }
 
 fn part_2() {
     let lines = list_of_strings_from_file();
-    let count = lines.iter().filter(|l| is_valid2(l)).count();
+    let count = lines.iter().filter(|l| is_valid2(l).unwrap()).count();
     println!("{}", count);
 }
 
@@ -94,8 +98,8 @@ mod tests {
 
     #[test]
     fn example_part_1() {
-        assert_eq!(is_valid("1-3 a: abcde"), true);
-        assert_eq!(is_valid("1-3 b: cdefg"), false);
-        assert_eq!(is_valid("2-9 c: ccccccccc"), true);
+        assert_eq!(is_valid("1-3 a: abcde"), Some(true));
+        assert_eq!(is_valid("1-3 b: cdefg"), Some(false));
+        assert_eq!(is_valid("2-9 c: ccccccccc"), Some(true));
     }
 }
