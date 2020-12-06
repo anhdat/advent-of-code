c94716e6625304ad58a7f3559a4cb28b32c87dc6
Dat Truong <mr.anhdat@gmail.com>
2020-12-06 08:30:08 +0100

    Make it shorter if we don't have special characters in multiple lines

--- 2020/day06/src/main.rs
+++ 2020/day06/src/main.rs
@@ -16,20 +16,10 @@ fn part_2(input: &str) {
     let sum_count: usize = input
         .split("\n\n")
         .map(|paragraph| {
-            let first_line = paragraph
+            let first_line: HashSet<char> = paragraph.lines().next().unwrap().chars().collect();
+            let common_chars: HashSet<char> = paragraph
                 .lines()
-                .next()
-                .unwrap()
-                .chars()
-                .filter(|c| c.is_alphabetic())
-                .collect::<HashSet<char>>();
-            let common_chars = paragraph
-                .lines()
-                .map(|line| {
-                    line.chars()
-                        .filter(|c| c.is_alphabetic())
-                        .collect::<HashSet<char>>()
-                })
+                .map(|line| line.chars().collect::<HashSet<char>>())
                 .fold(first_line, |acc, x| acc.intersection(&x).cloned().collect());
 
             common_chars.len()
