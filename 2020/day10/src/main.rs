2c7cceff7e51ac42c39a3f305683463757df15e7
Dat Truong <mr.anhdat@gmail.com>
2020-12-10 08:37:49 +0100

    Day 10 part 2
    with memorization

--- 2020/day10/src/main.rs
+++ 2020/day10/src/main.rs
@@ -1,3 +1,38 @@
+use std::collections::HashMap;
+
+fn count(nums: &Vec<isize>, current_index: usize, memory: &mut HashMap<usize, usize>) -> usize {
+    if memory.contains_key(&current_index) {
+        return *memory.get(&current_index).unwrap();
+    }
+
+    if nums.len() - 1 == current_index {
+        memory.insert(current_index, 1);
+        return 1;
+    }
+
+    let mut total = 0;
+
+    for diff in 1..=3 {
+        let next_num = nums[current_index] + diff;
+        if nums.contains(&next_num) {
+            let next_index = nums.iter().position(|&x| x == next_num).unwrap();
+            total += count(nums, next_index, memory);
+        }
+    }
+
+    memory.insert(current_index, total);
+    return total;
+}
+
+fn part_2(input: &str) {
+    let mut nums: Vec<isize> = input.lines().map(|l| l.parse::<isize>().unwrap()).collect();
+    let max_num: isize = *nums.iter().clone().max().unwrap();
+    nums = [nums, vec![0 as isize, max_num + 3]].concat();
+    nums.sort();
+    let mut memory: HashMap<usize, usize> = HashMap::new();
+    println!("{}", count(&nums, 0, &mut memory))
+}
+
 fn part_1(input: &str) {
     let mut nums: Vec<isize> = input.lines().map(|l| l.parse::<isize>().unwrap()).collect();
     nums.sort();
@@ -35,4 +70,6 @@ fn main() {
 
     part_1(&input_example);
     part_1(&input);
+    part_2(&input_example);
+    part_2(&input);
 }
