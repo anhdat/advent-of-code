84c33210a4cb6f2951e4027294faaaf9908bb5e6
Dat Truong <mr.anhdat@gmail.com>
2020-12-16 10:10:30 +0100

    Refactor document parsing

--- 2020/day16/src/main.rs
+++ 2020/day16/src/main.rs
@@ -1,57 +1,91 @@
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::HashMap;
+use std::ops::RangeInclusive;
 
 lazy_static! {
     static ref RE_FIELD: Regex = Regex::new(r"([\d\w]+): (\d+)-(\d+) or (\d+)-(\d+)").unwrap();
 }
 
-fn parse_document(input: &str) -> Option<()> {
+#[derive(Debug)]
+struct Field {
+    name: String,
+    ranges: Vec<std::ops::RangeInclusive<usize>>,
+}
+
+#[derive(Debug)]
+struct Ticket {
+    nums: Vec<usize>,
+}
+
+impl Ticket {
+    fn from_str(s: &str) -> Self {
+        let nums: Vec<usize> = s.split(',').map(|s| s.parse::<usize>().unwrap()).collect();
+        Ticket { nums: nums }
+    }
+}
+
+fn parse_document(input: &str) -> (std::vec::Vec<Field>, Ticket, std::vec::Vec<Ticket>) {
     let mut paragraphs = input.split("\n\n");
-    let fields: Vec<std::ops::RangeInclusive<usize>> = paragraphs
+    let fields: Vec<Field> = paragraphs
         .next()
         .unwrap()
         .lines()
-        .flat_map(|l| {
+        .map(|l| {
             let comps = RE_FIELD.captures(l).unwrap();
 
-            vec![
-                (comps.get(2).unwrap().as_str().parse::<usize>().unwrap()
-                    ..=comps.get(3).unwrap().as_str().parse::<usize>().unwrap()),
-                (comps.get(4).unwrap().as_str().parse::<usize>().unwrap()
-                    ..=comps.get(5).unwrap().as_str().parse::<usize>().unwrap()),
-            ]
+            Field {
+                name: comps.get(1).unwrap().as_str().to_string(),
+                ranges: vec![
+                    (comps.get(2).unwrap().as_str().parse::<usize>().unwrap()
+                        ..=comps.get(3).unwrap().as_str().parse::<usize>().unwrap()),
+                    (comps.get(4).unwrap().as_str().parse::<usize>().unwrap()
+                        ..=comps.get(5).unwrap().as_str().parse::<usize>().unwrap()),
+                ],
+            }
         })
         .collect();
+    // println!("{:?}", fields);
 
-    paragraphs.next();
-    let mut nearbys_raw = paragraphs.next()?.lines();
-    nearbys_raw.next();
-    let scan_results: Vec<usize> = nearbys_raw
-        .map(|l| {
-            let nums: Vec<usize> = l.split(',').map(|s| s.parse().unwrap()).collect();
-            let invalid_num = nums
-                .into_iter()
-                .skip_while(|num| fields.iter().any(|f| f.contains(num)))
+    let mut p2 = paragraphs.next().unwrap().lines();
+    p2.next();
+    let your_ticket: Ticket = Ticket::from_str(p2.next().unwrap());
+    // println!("{:?}", your_ticket);
+
+    let mut p3 = paragraphs.next().unwrap().lines();
+    p3.next();
+    let nearbys: Vec<Ticket> = p3.map(|l| Ticket::from_str(l)).collect();
+    // println!("{:?}", nearbys);
+
+    (fields, your_ticket, nearbys)
+}
+
+fn part_1(input: &str) {
+    let (fields, _, nearbys) = parse_document(&input);
+    let all_ranges: Vec<&RangeInclusive<usize>> = fields
+        .iter()
+        .flat_map(|f| f.ranges.iter().collect::<Vec<&RangeInclusive<usize>>>())
+        .collect();
+    let scan_results: Vec<usize> = nearbys
+        .iter()
+        .map(|t| {
+            let invalid_num: Option<&usize> = t
+                .nums
+                .iter()
+                .skip_while(|&num| all_ranges.iter().any(|range| range.contains(num)))
                 .next();
             match invalid_num {
-                Some(val) => val,
+                Some(val) => *val,
                 None => 0,
             }
         })
         .collect();
 
     println!("{:?}", scan_results.iter().sum::<usize>());
-
-    Some(())
 }
 
 fn main() {
     let input = include_str!("../input");
     // let input = include_str!("../example");
-    // let input = include_str!("../example_2");
-    // part_1(&input);
+    part_1(&input);
     // part_2(&input);
-    parse_document(&input).unwrap();
-    // println!("Hello, world!");
 }
