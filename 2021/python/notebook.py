3763b76f3b3aadfcd10d2972604b0e1db6472e91
Dat Truong <mr.anhdat@gmail.com>
2021-12-11 14:43:03 +0100

    Day 11 part 2

--- 2021/python/notebook.py
+++ 2021/python/notebook.py
@@ -13,6 +13,7 @@ import math
 import ast
 import sys
 import re
+import copy
 
 # Parsing
 
@@ -739,45 +740,51 @@ do(10, 339477, 3049320156)
 # Day 11
 
 directions = [
-    (0, 1), (1, 0), (0, -1), (-1, 0),
-    (1, 1), (-1, -1), (1, -1), (-1, 1),
+    (0, 1),
+    (1, 0),
+    (0, -1),
+    (-1, 0),
+    (1, 1),
+    (-1, -1),
+    (1, -1),
+    (-1, 1),
 ]
 in11: list[list[int]] = data(11, lambda v: [int(a) for a in v])
 
 Point = tuple[int, int]
 Table = list[list[int]]
-from pprint import pprint
 
-def next(m: Table) ->  int:
+
+def next(m: Table) -> int:
     q = deque()
-    for r in range(len(m)):
-        for c in range(len(m[0])):
-            m[r][c] += 1
-            if m[r][c] > 9:
-                q.append((c, r))
+    for (r, c) in product(range(len(m)), range(len(m[0]))):
+        m[r][c] += 1
+        if m[r][c] > 9:
+            q.append((c, r))
 
     flashes = 0
     while q:
         (c, r) = q.pop()
-        if m[r][c] > 9:
-            flashes += 1
-            m[r][c] = 0
-
-            for (dx, dy) in directions:
-                x = c + dx
-                y = r + dy
-                if 0 <= x < len(m[0]) and 0 <= y < len(m):
-                    if m[y][x] <= 9 and m[y][x] != 0:
-                        m[y][x] += 1
-                        if m[y][x] > 9:
-                            q.append((x, y))
+        flashes += 1
+        m[r][c] = 0
+        for (dx, dy) in directions:
+            x = c + dx
+            y = r + dy
+            if 0 <= x < len(m[0]) and 0 <= y < len(m):
+                if m[y][x] <= 9 and m[y][x] != 0:
+                    m[y][x] += 1
+                    if m[y][x] > 9:
+                        q.append((x, y))
     return flashes
 
+
 def day11_1(m: Table) -> int:
+    m = copy.deepcopy(m)
     c = 0
     for _ in range(100):
         c += next(m)
     return c
+
 
 m = """5483143223
 2745854711
@@ -789,6 +796,17 @@ m = """5483143223
 6882881134
 4846848554
 5283751526""".splitlines()
-m = [[int(i) for i in l] for l in m ]
+m = [[int(i) for i in l] for l in m]
 assert day11_1(m) == 1656
-do(11, 1603)
+
+
+def day11_2(m: Table) -> int:
+    c = 0
+    i = 0
+    while c != len(m) * len(m[0]):
+        c = next(m)
+        i += 1
+    return i
+
+
+do(11, 1603, 222)
