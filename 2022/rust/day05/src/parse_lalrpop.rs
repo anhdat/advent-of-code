6eb4c479126970821bb91b2639424c7a80b7937e
Dat Truong <2699721+anhdat@users.noreply.github.com>
2022-12-07 07:36:12 +0100

    Now can parse empty crate but not list of commands

--- 2022/rust/day05/src/parse_lalrpop.rs
+++ 2022/rust/day05/src/parse_lalrpop.rs
@@ -6,8 +6,15 @@ fn test_parsing() {
     // let lexer = crate::lexer::Lexer::new(input);
     // let a = ship::NumParser::new().parse(input, lexer);
     // println!("{:?}", a);
-    // let a = ship::ComParser::new().parse(input, lexer);
-    // println!("{:?}", a);
+    let a = ship::ComParser::new().parse("move 1 from 2 to 1");
+    println!("{:?}", a);
+    let a = ship::ListOfCommandsParser::new().parse(
+        "move 1 from 2 to 0
+move 3 from 1 to 2
+move 2 from 2 to 4
+move 1 from 1 to 6",
+    );
+    println!("{:?}", a);
     let a = ship::CratParser::new().parse("[R]");
     println!("{:?}", a);
     let a = ship::CratParser::new().parse("   ");
@@ -15,5 +22,13 @@ fn test_parsing() {
     let a = ship::LineParser::new().parse("[R] [A] [I]");
     println!("{:?}", a);
     let a = ship::LineParser::new().parse("    [A] [I]");
+    println!("{:?}", a);
+    let a = ship::ShipParser::new().parse(
+        "    [D]
+[N] [C]
+[Z] [M] [P]",
+    );
+    println!("{:?}", a);
+    let a = ship::InputParser::new().parse(crate::TESTING_INPUT);
     println!("{:?}", a);
 }
